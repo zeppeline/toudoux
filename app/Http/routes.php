1074fdaaf3edd4066d56bbebe84036155c46fb71
Céline Eppe <celine.eppe@gmail.com>
2016-08-28 13:51:15 +0200

    add the tasks route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'auth' ], function () {
+    Route::get('/tasks', 'TaskController@view');
+} );
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');

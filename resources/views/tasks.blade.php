f4632bcdee392896ab839c74a34f0eaeed5cfc14
Céline Eppe <celine.eppe@gmail.com>
2016-08-30 20:56:27 +0200

    add messages for empty data

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -28,26 +28,32 @@
                         </fieldset>
                     </form>
                     <!-- Projects list -->
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <a href="/tasks">All</a>
-                        </li>
-                        @foreach($projects as $project)
+                    @if (count($projects) == 0)
+                        <p>
+                            You don't have any projects yet but feel free to create some!
+                        </p>
+                    @else
+                        <ul class="list-group">
                             <li class="list-group-item">
-                                <a class="btn btn-default" href="/projects/{{ $project->id }}/confirmdelete">
-                                    <span class="glyphicon glyphicon-remove"></span>
-                                </a>
-                                <a class="btn btn-default" href="/api/projects/{{ $project->id }}/edit">
-                                    edit
-                                </a>
-                                <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
-                                <a href="?project={{ $project->id }}">{{ $project->name }}</a>
+                                <a href="/tasks">All</a>
                             </li>
-                        @endforeach
-                    </ul>
+                                @foreach ($projects as $project)
+                                    <li class="list-group-item">
+                                        <a class="btn btn-default" href="/projects/{{ $project->id }}/confirmdelete">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </a>
+                                        <a class="btn btn-default" href="/api/projects/{{ $project->id }}/edit">
+                                            edit
+                                        </a>
+                                        <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
+                                        <a href="?project={{ $project->id }}">{{ $project->name }}</a>
+                                    </li>
+                                @endforeach
+                        </ul>
+                    @endif
                 </div>
             </div>
-            <div class="panel panel-default">
+            <div class="panel panel-default" id="tags">
                 <div class="panel-heading">
                     My tags
                 </div>
@@ -73,25 +79,31 @@
                     <!-- Tags list -->
                     <form class="" action="/tasks" method="get">
                         <div>
-                            <ul class="list-group">
-                                @foreach($tags as $tag)
-                                <li class="list-group-item row">
-                                    <a class="btn btn-default" href="/tags/{{ $tag->id }}/confirmdelete">
-                                        <span class="glyphicon glyphicon-remove"></span>
-                                    </a>
-                                    <a class="btn btn-default" href="/api/tags/{{ $tag->id }}/edit">
-                                        edit
-                                    </a>
-                                    <div class="checkbox">
-                                        <label for="tag{{ $tag->id }}">
-                                            <input type="checkbox" name="tags[]" value="{{ $tag->id }}" id="tag{{ $tag->id }}">
-                                            {{ $tag->name }}
-                                        </label>
-                                    </div>
-                                </li>
-                                @endforeach
-                                <input type="submit" value="Sort tags" class="btn btn-primary">
-                            </ul>
+                            @if (count($tags) == 0)
+                                <p>
+                                    You don't have any tags yet but feel free to create some!
+                                </p>
+                            @else
+                                <ul class="list-group">
+                                    @foreach ($tags as $tag)
+                                    <li class="list-group-item row">
+                                        <a class="btn btn-default" href="/tags/{{ $tag->id }}/confirmdelete">
+                                            <span class="glyphicon glyphicon-remove"></span>
+                                        </a>
+                                        <a class="btn btn-default" href="/api/tags/{{ $tag->id }}/edit">
+                                            edit
+                                        </a>
+                                        <div class="checkbox">
+                                            <label for="tag{{ $tag->id }}">
+                                                <input type="checkbox" name="tags[]" value="{{ $tag->id }}" id="tag{{ $tag->id }}">
+                                                {{ $tag->name }}
+                                            </label>
+                                        </div>
+                                    </li>
+                                    @endforeach
+                                    <input type="submit" value="Sort tags" class="btn btn-primary">
+                                </ul>
+                            @endif
                         </div>
                     </form>
                 </div>
@@ -148,58 +160,70 @@
                                 </select>
                             </div>
                             <!-- Add tags (non mandatory) -->
-                            <div class="form-group col-md-12">
-                                <label class="col-md-12">Add tag(s):</label>
-                                @foreach($tags as $tag)
-                                    <label for="newtag{{ $tag->id }}">
-                                        <input type="checkbox" name="tags[]" id="newtag{{ $tag->id }}" value="{{ $tag->id }}">
-                                        {{ $tag->name }}
-                                    </label>
-                                @endforeach
-                            </div>
+                            @if(count($tags) == 0)
+                                <p>
+                                    <a href="#tags">No tags yet, add some!</a>
+                                </p>
+                            @else
+                                <div class="form-group col-md-12">
+                                    <label class="col-md-12">Add tag(s):</label>
+                                    @foreach ($tags as $tag)
+                                        <label for="newtag{{ $tag->id }}">
+                                            <input type="checkbox" name="tags[]" id="newtag{{ $tag->id }}" value="{{ $tag->id }}">
+                                            {{ $tag->name }}
+                                        </label>
+                                    @endforeach
+                                </div>
+                            @endif
                             <div class="form-group col-md-12">
                                 <input class="btn btn-primary" type="submit" value="Add new task">
                             </div>
                         </fieldset>
                     </form>
                     <!-- Tasks list -->
-                    <form class="no-js" action="/tasks" method="post">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="_method" value="PUT">
-                        <fieldset>
-                            <ul class="list-group">
-                                @foreach($tasks as $task)
-                                    <li class="form-group list-group-item row">
-                                        <input type="hidden" name="{{ $task->id }}" value="0">
-                                        <div class="col-md-3">
-                                            <button class="btn btn-default" type="submit" name="{{ 'delete-' . $task->id }}">
-                                                <span class="glyphicon glyphicon-remove"></span>
-                                            </button>
-                                            <input class="btn btn-default" type="submit" name="{{ 'edit-' . $task->id }}" value="edit">
-                                        </div>
-                                        <div class="checkbox col-md-9">
-                                            <label for="{{ $task->id }}"><input type="checkbox" name="{{ $task->id }}" id="{{ $task->id }}" value="1" {{ $task->completed == 1 ? 'checked="checked"' : '' }}>{{ $task->body }} {{ $task->due_date ? '(due ' . $task->due_date->diffForHumans() . ')' : '' }}</label>
-                                            <!-- Display tags -->
-                                            <p>
-                                                @foreach($task->tags as $tag)
-                                                    <a href="/tasks?tags[]={{ $tag->id }}">#{{ $tag->name }}</a>
-                                                @endforeach
-                                            </p>
-                                            <!-- Display project -->
-                                            @if($task->project()->first())
+                    @if(count($tasks) == 0)
+                        <p>
+                            No task to display yet.
+                        </p>
+                    @else
+                        <form class="no-js" action="/tasks" method="post">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="PUT">
+                            <fieldset>
+                                <ul class="list-group">
+                                    @foreach ($tasks as $task)
+                                        <li class="form-group list-group-item row">
+                                            <input type="hidden" name="{{ $task->id }}" value="0">
+                                            <div class="col-md-3">
+                                                <button class="btn btn-default" type="submit" name="{{ 'delete-' . $task->id }}">
+                                                    <span class="glyphicon glyphicon-remove"></span>
+                                                </button>
+                                                <input class="btn btn-default" type="submit" name="{{ 'edit-' . $task->id }}" value="edit">
+                                            </div>
+                                            <div class="checkbox col-md-9">
+                                                <label for="{{ $task->id }}"><input type="checkbox" name="{{ $task->id }}" id="{{ $task->id }}" value="1" {{ $task->completed == 1 ? 'checked="checked"' : '' }}>{{ $task->body }} {{ $task->due_date ? '(due ' . $task->due_date->diffForHumans() . ')' : '' }}</label>
+                                                <!-- Display tags -->
                                                 <p>
-                                                    <?php $project = $task->project()->first(); ?>
-                                                    <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
-                                                    <a href="?project={{ $task->project_id }}">{{ $project->name }}</a>
+                                                    @foreach ($task->tags as $tag)
+                                                        <a href="/tasks?tags[]={{ $tag->id }}">#{{ $tag->name }}</a>
+                                                    @endforeach
                                                 </p>
-                                            @endif
-                                        </div>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </fieldset>
-                        <input class="btn btn-primary" type="submit" value="Save changes">
-                    </form>
+                                                <!-- Display project -->
+                                                @if($task->project()->first())
+                                                    <p>
+                                                        <?php $project = $task->project()->first(); ?>
+                                                        <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
+                                                        <a href="?project={{ $task->project_id }}">{{ $project->name }}</a>
+                                                    </p>
+                                                @endif
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </fieldset>
+                            <input class="btn btn-primary" type="submit" value="Save changes">
+                        </form>
+                    @endif
                 </div>
             </div>
         </div>

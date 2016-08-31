b560757b11bcc3c843ed90ead515b388a6c5ec2b
Céline Eppe <celine.eppe@gmail.com>
2016-08-31 21:27:33 +0200

    rework templates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Toudoux</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700"> -->
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="css/app.css" rel="stylesheet">
 
-    <style>
+    <!--<style>
         body {
             font-family: 'Lato';
         }
@@ -23,52 +23,34 @@
         .fa-btn {
             margin-right: 6px;
         }
-    </style>
+    </style>-->
 </head>
 <body id="app-layout"class="@yield('bodyClass')">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+    <nav class="navbar">
+        <div class="navbar__container">
+            <a class="navbar__header" href="{{ url('/home') }}" title="Home">
+                <img class="navbar__header__logo" src="./img/logo.svg" alt="Toudoux" />
+                <h1 class="navbar__header__text">Toudoux</h1>
+            </a>
+            <ul class="navbar__items">
+                <li class="navbar__item"><a class="navbar__link" href="{{ url('/') }}">Home</a></li>
+                @if (Auth::guest())
+                    <li class="navbar__item"><a class="navbar__link" href="{{ url('/login') }}">Login</a></li>
+                    <li class="navbar__item"><a class="navbar__link" href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="navbar__item"><a class="navbar__link" href="{{ url('/tasks') }}">My tasks</a></li>
+                    <li class="navbar__item"><a class="navbar__link" href="{{ url('/logout') }}">Logout</a></li>
+                    <!-- <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li> -->
+                @endif
+            </ul>
         </div>
     </nav>
 
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -7,113 +7,103 @@ tasks @endsection
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">My projects</h3>
-                </div>
-                <div class="panel-body">
-                    <!-- New project form -->
-                    <form class="row no-js" action="/api/projects" method="post">
-                        {{ csrf_field() }}
-                        <fieldset>
-                            <div class="form-group col-md-12">
-                                <label for="newProject">New project:</label>
-                                <input class="form-control" type="text" name="projectName" value="" id="newProject">
-                                @if ($errors->has('projectName'))
-                                    <div>
-                                        <?php echo $errors->first('projectName'); ?>
-                                    </div>
-                                @endif
+            <div class="projects-panel">
+                <h2 class="panel__title">My projects</h2>
+                <!-- New project form -->
+                <form class="form" action="/api/projects" method="post">
+                    {{ csrf_field() }}
+                    <fieldset class="form__small">
+                        <label for="newProject" class="form__label">Add project</label>
+                        <input class="form__input" type="text" name="projectName" value="" placeholder="Your project name" id="newProject">
+                        @if ($errors->has('projectName'))
+                            <div>
+                                <?php echo $errors->first('projectName'); ?>
                             </div>
-                            <div class="form-group col-md-12">
-                                <input class="btn btn-primary" type="submit" value="Add new project">
-                            </div>
-                        </fieldset>
-                    </form>
-                    <!-- Projects list -->
-                    @if (count($projects) == 0)
-                        <p>
-                            You don't have any projects yet but feel free to create some!
-                        </p>
-                    @else
-                        <ul class="list-group">
-                            <li class="list-group-item">
+                        @endif
+                        <input class="form__button primary" type="submit" value="Add">
+                    </fieldset>
+                </form>
+                <!-- Projects list -->
+                @if (count($projects) == 0)
+                    <p class="no-item">
+                        You don't have any projects yet but feel free to create some!
+                    </p>
+                @else
+                    <ul class="list">
+                            @foreach ($projects as $project)
+                                <li class="list__item">
+                                    <a class="list__item__item" href="?project={{ $project->id }}">
+                                        <span class="project-icon" style="background-color: {{ $project->color }};"></span>
+                                        {{ $project->name }}
+                                    </a>
+                                    <a class="list__item__delete action-icon" href="/projects/{{ $project->id }}/confirmdelete">
+                                        <i class="fa fa-times" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="list__item__edit action-icon" href="/api/projects/{{ $project->id }}/edit">
+                                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            @endforeach
+                            <li class="list__item">
+                                <span class="project-icon" ></span>
                                 <a href="/tasks">All</a>
                             </li>
-                                @foreach ($projects as $project)
-                                    <li class="list-group-item">
-                                        <a class="btn btn-default" href="/projects/{{ $project->id }}/confirmdelete">
-                                            <span class="glyphicon glyphicon-remove"></span>
-                                        </a>
-                                        <a class="btn btn-default" href="/api/projects/{{ $project->id }}/edit">
-                                            edit
-                                        </a>
-                                        <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
-                                        <a href="?project={{ $project->id }}">{{ $project->name }}</a>
-                                    </li>
-                                @endforeach
-                        </ul>
-                    @endif
-                </div>
+                    </ul>
+                @endif
             </div>
-            <div class="panel panel-default" id="tags">
-                <div class="panel-heading">
-                    My tags
-                </div>
-                <div class="panel-body">
-                    <!-- New tag form -->
-                    <form class="row no-js" action="/api/tags" method="post">
-                        {{ csrf_field() }}
-                        <fieldset>
-                            <div class="form-group col-md-12">
-                                <label for="tagName">New tag:</label>
-                                <input class="form-control" type="text" name="tagName" value="" id="tagName">
-                                @if ($errors->has('tagName'))
-                                    <div>
-                                        <?php echo $errors->first('tagName'); ?>
+            <div class="tags-panel" id="tags">
+                <h2 class="panel__title">My tags</h2>
+                <!-- New tag form -->
+                <form class="form" action="/api/tags" method="post">
+                    {{ csrf_field() }}
+                    <fieldset class="form__small">
+                        <label class="form__label" for="tagName">Add tag</label>
+                        <input class="form__input" type="text" name="tagName" placeholder="Your tag (ex: design)" value="" id="tagName">
+                        @if ($errors->has('tagName'))
+                            <div>
+                                <?php echo $errors->first('tagName'); ?>
+                            </div>
+                        @endif
+                        <input class="form__button primary" type="submit" value="Add">
+                    </fieldset>
+                </form>
+                <!-- Tags list -->
+                <form class="" action="/tasks" method="get">
+                    <div>
+                        @if (count($tags) == 0)
+                            <p>
+                                You don't have any tags yet but feel free to create some!
+                            </p>
+                        @else
+                            <ul class="list">
+                                @foreach ($tags as $tag)
+                                <li class="list__item">
+                                    <div class="checkbox">
+                                        <label for="tag{{ $tag->id }}">
+                                            <input type="checkbox" name="tags[]" value="{{ $tag->id }}" id="tag{{ $tag->id }}">
+                                            {{ $tag->name }}
+                                        </label>
                                     </div>
-                                @endif
-                            </div>
-                            <div class="form-group col-md-12">
-                                <input class="btn btn-primary" type="submit" value="Add new tag">
-                            </div>
-                        </fieldset>
-                    </form>
-                    <!-- Tags list -->
-                    <form class="" action="/tasks" method="get">
-                        <div>
-                            @if (count($tags) == 0)
-                                <p>
-                                    You don't have any tags yet but feel free to create some!
-                                </p>
-                            @else
-                                <ul class="list-group">
-                                    @foreach ($tags as $tag)
-                                    <li class="list-group-item row">
-                                        <a class="btn btn-default" href="/tags/{{ $tag->id }}/confirmdelete">
-                                            <span class="glyphicon glyphicon-remove"></span>
-                                        </a>
-                                        <a class="btn btn-default" href="/api/tags/{{ $tag->id }}/edit">
-                                            edit
-                                        </a>
-                                        <div class="checkbox">
-                                            <label for="tag{{ $tag->id }}">
-                                                <input type="checkbox" name="tags[]" value="{{ $tag->id }}" id="tag{{ $tag->id }}">
-                                                {{ $tag->name }}
-                                            </label>
-                                        </div>
-                                    </li>
-                                    @endforeach
-                                    <input type="submit" value="Sort tags" class="btn btn-primary">
-                                </ul>
-                            @endif
-                        </div>
-                    </form>
-                </div>
+                                    <a class="list__item__delete action-icon" href="/tags/{{ $tag->id }}/confirmdelete">
+                                        <i class="fa fa-times" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="list__item__edit action-icon" href="/api/tags/{{ $tag->id }}/edit">
+                                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                @endforeach
+                                <!-- <fieldset class="form__small"> -->
+                                    <!-- <span class="form__info">Check the tags you want to sort your tasks by</span> -->
+                                    <input type="submit" value="Sort" class="form__button primary">
+                                <!-- </fieldset> -->
+                            </ul>
+                        @endif
+                    </div>
+                </form>
             </div>
         </div>
         <div class="col-md-8">
-            <div class="panel panel-default">
+            <div class="tasks-panel panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">My tasks</h3>
                     <!-- Sort by date -->
@@ -232,4 +222,33 @@ tasks @endsection
         </div>
     </div>
 </div>
+<template id="taskDisplay">
+    <li class="form-group list-group-item row">
+        <input type="hidden" name="{{ $task->id }}" value="0">
+        <div class="col-md-3">
+            <button class="btn btn-default" type="submit" name="{{ 'delete-' . $task->id }}">
+                <span class="glyphicon glyphicon-remove"></span>
+            </button>
+            <input class="btn btn-default" type="submit" name="{{ 'edit-' . $task->id }}" value="edit">
+        </div>
+        <div class="checkbox col-md-9">
+            <label for="{{ $task->id }}"><input type="checkbox" name="{{ $task->id }}" id="{{ $task->id }}" value="1" {{ $task->completed == 1 ? 'checked="checked"' : '' }}>{{ $task->body }} {{ $task->due_date ? '(due ' . $task->due_date->diffForHumans() . ')' : '' }}</label>
+            <!-- Display tags -->
+            <p>
+                @foreach ($task->tags as $tag)
+                    <a href="/tasks?tags[]={{ $tag->id }}">#{{ $tag->name }}</a>
+                @endforeach
+            </p>
+            <!-- Display project -->
+            @if($task->project()->first())
+                <p>
+                    <?php $project = $task->project()->first(); ?>
+                    <span style="display: inline-block; width: 16px; height: 16px; border-radius: 50%; background-color: {{ $project->color }}; margin-right: 10px; vertical-align: middle;"></span>
+                    <a href="?project={{ $task->project_id }}">{{ $project->name }}</a>
+                </p>
+            @endif
+        </div>
+    </li>
+</template>
+
 @endsection
